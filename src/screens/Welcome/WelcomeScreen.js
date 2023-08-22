7758e17d72aa6ae8c83c242c09128a6496159897
Yuri Castilho <yuricastilho2005@gmail.com>
2023-08-21 23:30:12 -0300

    estrutura WelcomeScreen completa

--- src/screens/Welcome/WelcomeScreen.js
+++ src/screens/Welcome/WelcomeScreen.js
@@ -1,11 +1,30 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import styles from './WelcomeStyles'
 
 export default function WelcomeScreen() {
   return (
-    <View>
-      <Text>WelcomeScreen</Text>
+    <View style={styles.main}>
+
+      <Image style={styles.bg_image}
+        source={require('../../../assets/images/welcome.png')}
+      />
+
+      <View style={styles.container}>
+        <View style={styles.texts}>
+          <Text style={styles.title}>
+            Viajar agora é fácil!
+          </Text>
+          <Text style={styles.description}>
+            Experimente das as melhores aventuras ao redor do mundo com a gente
+          </Text>
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.button_text}>
+            Vamos Lá!
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
